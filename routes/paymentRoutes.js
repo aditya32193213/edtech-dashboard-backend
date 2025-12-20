49c6e801a182ff59dbe2411d63ebdf00ab9c45ff
Aditya Kumar <aditya32193213@gmail.com>
2025-12-20 10:38:31 +0530

    Final changes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,23 @@
+// const express = require("express");
+// const router = express.Router();
+// const { createCheckoutSession } = require("../controllers/paymentController");
+
+// router.post("/checkout", createCheckoutSession);
+
+// module.exports = router;
+
+
+
+
+
 const express = require("express");
 const router = express.Router();
 const { createCheckoutSession } = require("../controllers/paymentController");
 
+// Existing route
 router.post("/checkout", createCheckoutSession);
+
+// ✅ Alias route (fixes frontend 404)
+router.post("/create-checkout-session", createCheckoutSession);
 
 module.exports = router;
