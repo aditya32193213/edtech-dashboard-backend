dda8299dab6fd27687ef80611bd9e8437113858f
Aditya Kumar <aditya32193213@gmail.com>
2026-01-19 02:05:31 +0530

    Finalize backend Sentry integration

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 require("events").EventEmitter.defaultMaxListeners = 20;
+
+const Sentry = require("./instrument");
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
+dotenv.config();
+
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const courseRoutes = require("./routes/courseRoutes");
@@ -11,11 +17,14 @@ const progressRoutes = require("./routes/progressRoutes");
 const { swaggerUi, swaggerDocument } = require("./config/swagger");
 const aiRoutes = require("./routes/aiRoutes");
 
-dotenv.config();
+
+
 connectDB();
 console.log("Gemini Key Loaded:", process.env.GEMINI_API_KEY);
 
 const app = express();
+
+
 const allowedOrigins = ["http://localhost:3000", "https://edtech-dashboard-frontend.vercel.app"];
 
 app.use(
@@ -44,6 +53,17 @@ app.use("/api/ai", aiRoutes);
 app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 
 
+// 👇 Sentry error handler (AFTER routes)
+Sentry.setupExpressErrorHandler(app);
+
+// 👇 fallback error handler
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Internal Server Error",
+     sentryEventId: res.sentry, // 👈 THIS LINE
+  });
+   next(err); // 👈 important
+});
 
 
 app.get("/", (req, res) => {
